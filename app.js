395e87c60b08338bfaaeeb061ef9168ecc8cd470
Shashank Singh <shashankrd1@gmail.com>
2018-01-11 14:28:48 +0530

    heroku saved

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port =process.env.PORT  |3000;
+var port =process.env.PORT  ||3000;
 app.set("view engine","ejs");
 app.use(express.static(__dirname+'/public'));
 
